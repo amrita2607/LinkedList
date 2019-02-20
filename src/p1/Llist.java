f75418bff37f20cb41b9023e3d008060545987d7
amrita2607 <amrita.agarwal_ccv17@gla.ac.in>
2019-02-20 11:07:46 +0530

    add getter and setter for attribute data

--- src/p1/Llist.java
+++ src/p1/Llist.java
@@ -3,5 +3,14 @@ package p1;
 public class Llist
 {
     private int data;
-    
+
+    public int getData()
+    {
+        return data;
+    }
+
+    public void setData(int data)
+    {
+        this.data = data;
+    }
 }
